f5d042fe55dd05f35b69248b3ccc99fb41a84378
unknown <rafaelrhernandes@outlook.com>
2024-09-15 05:48:14 -0300

    feat: aulas 11 e 112

--- Curso_Python/aula111.py
+++ Curso_Python/aula111.py
@@ -8,6 +8,6 @@
 # O python conhece todos os módulos e pacotes presentes
 # nos caminhos de sys.path
 
-import aula109
+import aula110
 
 print('Este módulo se chama', __name__)
---
+++ Curso_Python/aula112.py
@@ -0,0 +1,3 @@
+# import é singleton = apenas 1 instância, por questão de eficiência, se já foi importado uma vez ele não vai importar de novo.
+
+import importlib 
